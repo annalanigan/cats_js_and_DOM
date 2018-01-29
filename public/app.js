4bcef2bb908764b4dff49f16a7e9dc97d8f1386d
annalanigan <annalanigan@outlook.com>
2018-01-29 15:31:51 +0000

    initial commit

---
+++ public/app.js
@@ -0,0 +1,11 @@
+// create method to call after DOM is loaded
+
+// create new ul of class "cats"
+
+// create new li for name
+
+// create new li for favourite food
+
+// create new li for image (watch the order)
+
+// add new cat
